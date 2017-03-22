478979ce2cc9d0f793d28bdaea6ad75ee95e225e
utopiacosmica <utopiacosmica@icloud.com>
2017-03-22 02:36:53 -0700

    t

--- main_test.py
+++ main_test.py
@@ -7,8 +7,8 @@ import os
 class MainTest(unittest.TestCase):
 
 	def setUp(self):
-		driver_path = os.path.realpath('chromedriver')
-		self.driver = webdriver.Chrome(driver_path)
+		# driver_path = os.path.realpath('chromedriver')
+		self.driver = webdriver.PhantomJS('/Users/utopia/GitHub/utopia_test/phantomjs')
 		self.driver.maximize_window()
 
 	def tearDown(self):
--- test_python_org.py
+++ test_python_org.py
@@ -7,11 +7,17 @@ class PythonOrgSiteSearch(main_test.MainTest):
 
     def test_(self):
         driver = self.driver
+        driver.implicitly_wait(2)
         driver.get("http://www.python.org")
+        driver.implicitly_wait(2)
         self.assertIn("Python", driver.title)
+        driver.implicitly_wait(2)
         elem = driver.find_element_by_name("q")
+        driver.implicitly_wait(2)
         elem.send_keys("pycon")
+        driver.implicitly_wait(2)
         elem.send_keys(Keys.RETURN)
+        driver.implicitly_wait(2)
         assert "No results found." not in driver.page_source
 
 
