2484427b948c7326b51dce304d48c20d8471e9c5
Roma <utopiacosmica@icloud.com>
2017-05-11 21:56:41 -0800

    some new

--- template.py
+++ template.py
@@ -1,6 +1,9 @@
 import unittest
 import main_test
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+import time
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 class Name(main_test.MainTest):
